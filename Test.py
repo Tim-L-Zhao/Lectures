7ba4aedf202edee44b60323e6135330b4e267fa9
Tim Zhao <77992714+Tim-L-Zhao@users.noreply.github.com>
2021-05-12 10:29:09 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,31 @@
-def factorial(x):
-    f = x
-    for ii in reversed(range(x)[1:]):
-        f = f*ii
-    return f
+import timeit
+import sys
+import LogisticMap as lm
+
+start = timeit.default_timer()
+l = lm.LogisticMap()
+l.iterate(5000000)
+stop = timeit.default_timer()
+total_time = stop - start
+# output running time in a nice format.
+sys.stdout.write("Regular: {} s \n".format(total_time))
+
+
+start = timeit.default_timer()
+lg = lm.LogisticMap()
+lg.iterate_gen(5000000)
+
+stop = timeit.default_timer()
+total_time = stop - start
+# output running time in a nice format.
+sys.stdout.write("generator: {} s \n".format(total_time))
+
+start = timeit.default_timer()
+li = lm.LogisticMap_iter()
+l = [next(li) for i in range(5000000)]
+stop = timeit.default_timer()
+total_time = stop - start
+
+# output running time in a nice format.
+
+sys.stdout.write("Iterator class: {} s \n".format(total_time))
